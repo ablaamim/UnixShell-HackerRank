ff4a5e6797718b30e86f66e688808ea302428b84
alaamimi <aku-no-hana@outlook.fr>
2021-09-08 15:35:30 +0100

    fractal tree

---
+++ Bash/functions_and_fractals_recursive_trees_bash.sh
@@ -0,0 +1,43 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    functions_and_fractals_recursive_trees_ba          :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: alaamimi <alaamimi@student.1337.ma>        +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2021/09/08 15:34:16 by alaamimi          #+#    #+#              #
+#    Updated: 2021/09/08 15:34:18 by alaamimi         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+#!/bin/bash
+
+declare -A a
+
+f()
+{
+    local d=$1 l=$2 r=$3 c=$4
+    [[ $d -eq 0 ]] && return
+    for ((i=l; i; i--)); do
+        a[$((r-i)).$c]=1
+    done
+    ((r -= l))
+    for ((i=l; i; i--)); do
+        a[$((r-i)).$((c-i))]=1
+        a[$((r-i)).$((c+i))]=1
+    done
+    f $((d-1)) $((l/2)) $((r-l)) $((c-l))
+    f $((d-1)) $((l/2)) $((r-l)) $((c+l))
+}
+read n
+f $n 16 63 49
+for ((i=0; i<63; i++)); do
+    for ((j=0; j<100; j++)); do
+        if [[ ${a[$i.$j]} ]]; then
+            printf 1
+        else
+            printf _
+        fi
+    done
+    echo
+done
